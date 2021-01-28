c2ef1efe0153a24f8708488c4a45f4354f1634a3
Avi Mizrahi <avi1mizrahi@gmail.com>
2021-01-28 09:38:54 +0200

    temp- fix last

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -818,7 +818,7 @@ TxMempoolActivityEntry::TxMempoolActivityEntry(const CTxMemPoolEntry& tx,
                                                Optional <MemPoolRemovalReason> reason) :
         idx(idx),
         txid(tx.GetSharedTx()->GetHash()),
-        m_time(reason ? GetTime() : tx.GetTime()),
+        m_time(reason ? GetTime<std::chrono::seconds>() : tx.GetTime()),
         fee(tx.GetFee()),
         nFeeDelta(tx.GetModifiedFee() - tx.GetFee()),
         vsize(tx.GetTxSize()),
