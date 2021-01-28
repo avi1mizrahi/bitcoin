4e904da5d647d4cb1bcc59c2d87bd2fc24150595
Avi Mizrahi <avi1mizrahi@gmail.com>
2021-01-28 10:00:39 +0200

    Flush mempool recording periodically

--- src/init.cpp
+++ src/init.cpp
@@ -2019,6 +2019,12 @@ bool AppInitMain(const util::Ref& context, NodeContext& node, interfaces::BlockA
         banman->DumpBanlist();
     }, DUMP_BANS_INTERVAL);
 
+    auto* mempool = node.mempool.get();
+    mempool->startRecordActivity();
+    node.scheduler->scheduleEvery([mempool]{
+        mempool->flushRecordActivity();
+    }, std::chrono::minutes{5});
+
 #if HAVE_SYSTEM
     StartupNotify(args);
 #endif
